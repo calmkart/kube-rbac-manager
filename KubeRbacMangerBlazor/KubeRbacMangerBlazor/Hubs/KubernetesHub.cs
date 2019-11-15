dcd51df9facf8f720a6b73612b97e6c62a4c326a
Leisurely Clouds <leisurelyclouds@gmail.com>
2019-11-15 23:38:27 +0800

    feat: list and watch roles from k8s

---
+++ KubeRbacMangerBlazor/KubeRbacMangerBlazor/Hubs/KubernetesHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace KubeRbacMangerBlazor.Hubs
+{
+    public class KubernetesHub : Hub
+    {
+        public static string ClientsString = "Clients";
+        private readonly KubernetesService kubernetesService;
+
+        public KubernetesHub(KubernetesService kubernetesService)
+        {
+            this.kubernetesService = kubernetesService;
+        }
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, ClientsString);
+        }
+
+        public async Task GetRoles(string @namespace)
+        {
+            await kubernetesService.GetRoles(@namespace);
+        }
+    }
+}
---
+++ KubeRbacMangerBlazor/KubeRbacMangerBlazor/KubernetesService.cs
@@ -0,0 +1,47 @@
+﻿using k8s;
+using k8s.Models;
+using KubeRbacMangerBlazor.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace KubeRbacMangerBlazor
+{
+    public class KubernetesService
+    {
+        private readonly IHubContext<KubernetesHub> hubContext;
+
+        public KubernetesService(IHubContext<KubernetesHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+
+        public async Task GetRoles(string @namespace)
+        {
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+
+            IKubernetes client = new Kubernetes(config);
+
+            var podlistResp = await client.ListNamespacedRoleWithHttpMessagesAsync(@namespace, watch: true);
+            podlistResp.Watch<V1Role, V1RoleList>(Watcher_OnEvent);
+        }
+
+        private async void Watcher_OnEvent(WatchEventType watchEventType, V1Role v1Role)
+        {
+            switch (watchEventType)
+            {
+                case WatchEventType.Added:
+                    await hubContext.Clients.Group(KubernetesHub.ClientsString).SendAsync("NotifyRoleAddEvent", v1Role.Metadata.Name);
+                    break;
+                case WatchEventType.Modified:
+                    break;
+                case WatchEventType.Deleted:
+                    await hubContext.Clients.Group(KubernetesHub.ClientsString).SendAsync("NotifyRoleDeleteEvent", v1Role.Metadata.Name);
+                    break;
+                case WatchEventType.Error:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
